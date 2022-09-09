231310c8895456cc2709165e579041e76840e701
ruben <112951397+fisk008@users.noreply.github.com>
2022-09-09 16:13:45 +0200

    python_basics/playsound.py

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,20 +1,18 @@
 import simpleaudio as sa
-i=1
-c=0
+i=0
 
-afspelen = input("hoe vaak wil je het afspelen?")
-c = afspelen
-#print("dat is het goede antwoord het wordt nu ",afspelen," afgespeeld!")
+username= input('hoi hoe heet je?')
+print("welkom",username," ik ga nu een audio bestand afspelen")
+afspelen = input("hoe vaak wil je het horen?")
 
-while i < 10 :
-    
-    print("het wordt nu voor de",i,"de keer afgespeeld")
-    print(i)
-    # wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
-    #play_obj = wave_obj.play()
-    #play_obj.wait_done()
-    if (i == c):
-        break
+int_afspelen = int(afspelen)
+print("dat is het goede antwoord het wordt nu ",int_afspelen,"keer afgespeeld!")
+int_afspelen + 1
+while i < int_afspelen :   
+    print("Het loopje wordt nu voor de",i+1,"de keer afgespeeld")
+    wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
     i=i+1
     
-#print("dat is het goede antwoord het wordt nu ",afspelen," afgespeeld!")
+print("hij is nu klaar met",int_afspelen,"keer afspelen!")
