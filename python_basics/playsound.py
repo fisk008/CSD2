8f7e5886bb5a63877a251744954fa168d33689c5
ruben <112951397+fisk008@users.noreply.github.com>
2022-09-09 15:35:46 +0200

    python_basics/p

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,20 @@
 import simpleaudio as sa
+i=1
+c=0
 
-wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+afspelen = input("hoe vaak wil je het afspelen?")
+c = afspelen
+#print("dat is het goede antwoord het wordt nu ",afspelen," afgespeeld!")
+
+while i < 10 :
+    
+    print("het wordt nu voor de",i,"de keer afgespeeld")
+    print(i)
+    # wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
+    #play_obj = wave_obj.play()
+    #play_obj.wait_done()
+    if (i == c):
+        break
+    i=i+1
+    
+#print("dat is het goede antwoord het wordt nu ",afspelen," afgespeeld!")
