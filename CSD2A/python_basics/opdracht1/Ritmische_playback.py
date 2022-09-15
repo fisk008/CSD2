c32db9a5153566a3226be5bc2440f3af8a2770cd
ruben <112951397+fisk008@users.noreply.github.com>
2022-09-15 14:04:35 +0200

    krijg de sleep libarary nog niet helemaal werkend

---
+++ CSD2A/python_basics/opdracht1/Ritmische_playback.py
@@ -0,0 +1,25 @@
+#gebruik een lijst en funcitie range
+
+import simpleaudio as sa
+import time 
+i=0
+
+playback = input("how many times would you like to hear it?")
+ritm = input('Enter space-separated : ').split()
+int_playback = int(playback)
+list_of_rithms = [float(item) for item in ritm]
+
+int_playback + 1
+  
+for i in range(int_playback) :
+    print(i+1)
+    wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    sleep(3)
+    i=i+1
+      
+
+
+
+
--- CSD2A/python_basics/opdracht1/rangeloop_test.py
+++ CSD2A/python_basics/opdracht1/rangeloop_test.py
@@ -1,5 +1,5 @@
 user_input = input('Enter space-separated integers: ').split()
 
-list_of_integers = [int(item) for item in user_input]
+list_of_integers = [float(item) for item in user_input]
 
 print(list_of_integers)
