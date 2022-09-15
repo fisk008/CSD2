49b0928eb344ee7d0b0e7b09458b477ee200ffa8
ruben <112951397+fisk008@users.noreply.github.com>
2022-09-15 14:48:46 +0200

    getting closer

--- CSD2A/python_basics/opdracht1/Ritmische_playback.py
+++ CSD2A/python_basics/opdracht1/Ritmische_playback.py
@@ -7,16 +7,16 @@ i=0
 playback = input("how many times would you like to hear it?")
 ritm = input('Enter space-separated : ').split()
 int_playback = int(playback)
-list_of_rithms = [float(item) for item in ritm]
-
+list_of_rithms = [int(item) for item in ritm]
+ 
 int_playback + 1
   
-for i in range(int_playback) :
+for i in range(int_playback):
     print(i+1)
     wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
     play_obj = wave_obj.play()
     play_obj.wait_done()
-    sleep(3)
+    time.sleep(loop)
     i=i+1
       
 
