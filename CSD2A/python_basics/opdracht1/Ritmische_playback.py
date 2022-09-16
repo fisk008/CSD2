8c9c4d13ab1a327455533a149cb5577299d20ec7
ruben <112951397+fisk008@users.noreply.github.com>
2022-09-16 10:07:19 +0200

    als het goed is ie nu af

--- CSD2A/python_basics/opdracht1/Ritmische_playback.py
+++ CSD2A/python_basics/opdracht1/Ritmische_playback.py
@@ -4,23 +4,21 @@ import simpleaudio as sa
 import time 
 i=0
 
-playback = input("how many times would you like to hear it?")
+playback = int(input("how many times would you like to hear it?"))
 ritm = input('Enter space-separated floats : ').split()
-int_playback = int(playback)
+inputbpm= int(input("bpm?:"))
+bpm = 60/inputbpm
 list_of_rithms = [float(item) for item in ritm]
- 
-int_playback + 1
-  
-for i in range(int_playback):
-    print("loop:",i+1,"     ","time betweem samples:",list_of_rithms[i])
+total_rithms= sum(list_of_rithms)
+playback + 1
+print(total_rithms)
+
+for i in range(playback):
+    print("loop:",i+1,"     ","time in ms betweem samples:",list_of_rithms[i]*bpm)
     wave_obj = sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/CSD2/audio_files/test1.wav")
     play_obj = wave_obj.play()
     play_obj.wait_done()
     list_of_rithms.append(1)
-    time.sleep(list_of_rithms[i])
+    time.sleep(list_of_rithms[i]*bpm)
     i=i+1
       
-
-
-
-
