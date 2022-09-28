18add6c59676952c359fb27f048a21b8d4471db4
ruben <112951397+fisk008@users.noreply.github.com>
2022-09-28 20:33:36 +0200

    made small adjustments

---
+++ CSD2A/python_basics/opdracht4/dictionary_test.py
@@ -0,0 +1,13 @@
+import simpleaudio as sa
+kick = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+
+
+
+samples = [ sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/HKU/jaar_2/CSD_22-23/blok2a/assets/Plop.wav"),
+            sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/HKU/jaar_2/CSD_22-23/blok2a/assets/snare.wav"),
+            sa.WaveObject.from_wave_file("/Users/rubenbos/Documents/HKU/jaar_2/CSD_22-23/blok2a/assets/kick.wav")]
