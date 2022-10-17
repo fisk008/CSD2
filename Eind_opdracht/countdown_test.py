786582e03b1d9fcbafafeec32fac2c86764fbf5e
ruben <112951397+fisk008@users.noreply.github.com>
2022-10-17 18:32:14 +0200

    test for countdown

--- Eind_opdracht/countdown_test.py
+++ Eind_opdracht/countdown_test.py
@@ -3,8 +3,10 @@ import pyautogui
 print('Starting in ', end=''); pyautogui.countdown(3)
 
 import pyautogui
-for i in range(10): # Move mouse in a square.
-    pyautogui.moveTo(100, 100, duration=0.25)
-    pyautogui.moveTo(200, 100, duration=0.25)
-    pyautogui.moveTo(200, 200, duration=0.25)
-    pyautogui.moveTo(100, 200, duration=0.25)
+fw = pyautogui.getActiveWindow(x,y)
+fw.width # Gets the current width of the window.
+1669
+fw.topleft # Gets the current position of the window.
+(174, 153)
+fw.width = 1000 # Resizes the width.
+fw.topleft = (800, 400) # Moves the window.
