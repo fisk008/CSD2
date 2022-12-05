14716c9f44f30e04ed904000bd638167d9ff9e39
ruben <112951397+fisk008@users.noreply.github.com>
2022-12-05 14:32:17 +0100

    added saw class

---
+++ CSD2B/c++/synth_vierkantje/saw.h
@@ -0,0 +1,20 @@
+#ifndef _SAW_H_
+#define _SAW_H_
+#include <iostream>
+#include <cmath>
+#include "oscillator.h"
+
+
+
+class Saw : public Oscillator
+{
+    public:
+    Saw();
+    Saw(float frequency, float ampliltude, float phase, float samplerate);
+    Saw(float frequency, float amplitude); 
+    ~Saw();
+    
+
+    void tick();
+};
+#endif
