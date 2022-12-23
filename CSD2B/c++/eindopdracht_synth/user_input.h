da1b467138575783dc5cd0c4ce73a6433dc9c506
ruben <112951397+fisk008@users.noreply.github.com>
2022-12-23 11:03:06 +0100

    Basale versie voor vakantie

--- CSD2B/c++/eindopdracht_synth/user_input.h
+++ CSD2B/c++/eindopdracht_synth/user_input.h
@@ -4,16 +4,15 @@
 class UserInput{
 public:
 
-UserInput();
     UserInput();
     ~UserInput();
 
     std::string retrieveUserInput(std::string selectionOptions[], int numOptions);
-    
+
     bool validateSelection(std::string selection, std::string selectionOptions[],int numOptions);
     int retrieveUserSelection(std::string selectionOptions[], int numOptions);
     float retrieveValueInRange(float min, float max);
     float retrieveBPMInRange(float min, float max);
-    ~UserInput();
+    
 
 };
