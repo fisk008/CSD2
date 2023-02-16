44d0f8cb8d3b82880093d06d8657e17ae1b1059b
ruben <112951397+fisk008@users.noreply.github.com>
2023-02-16 14:06:04 +0100

    update

--- CSD2C/cpp_opdrachten/effecten/main.cpp
+++ CSD2C/cpp_opdrachten/effecten/main.cpp
@@ -8,20 +8,21 @@ class Callback : public AudioCallback {
 
 public:
     void prepare (int sampleRate) override {
-        for (Sine& sine : sines)
+        for (Sine& sine : sines){
             sine.prepareToPlay (static_cast<double> (sampleRate));
-        for (Tremolo& tremolo : tremolos)
+            }
+        for (Tremolo& tremolo : tremolos){
             tremolo.prepareToPlay (static_cast<double> (sampleRate));
-
+        }
     }
-
 
     void process (AudioBuffer buffer) override {
         auto [inputChannels, outputChannels, numInputChannels, numOutputChannels, numFrames] = buffer;
 
         for (int channel = 0u; channel < numOutputChannels; ++channel) {
             for (int sample = 0u; sample < numFrames; ++sample) {
-                outputChannels[channel][sample] = tremolos[channel].output(sines[channel].output());
+                outputChannels[channel][sample] = sines[channel].output();
+                // if (sample % 44100 == 0)std::cout << sines[channel].output();
             }
         }
     }
